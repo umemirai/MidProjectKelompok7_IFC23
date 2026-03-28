d317dc802a66aeac1e1b9ea1747e1009a339c552
Kiki Pangestu <99467638+umemirai@users.noreply.github.com>
2026-03-28 08:39:15 +0700

    Mengahpus halaman Login

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,8 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', function () {
-    return view('pages.auth.login');
-});
-
-Route::get('/admin/dashboard', function () {
+// Langsung memanggil halaman dashboard saat project dijalankan
+Route::get('/', function () {
     return view('pages.admin.dashboard');
 });
-
-Route::redirect('/', '/login');
